fb8fadf3b3f5d7bfae0bdecbe8afe5dad67c4d05
Daniel Estocado <estocado.daniel143@gmail.com>
2025-07-07 10:34:57 +0800

    fix the router

--- src/main.jsx
+++ src/main.jsx
@@ -8,16 +8,18 @@ import AllContacts from './pages/AllContacts.jsx'
 import AllAbout from './pages/AllAbout.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-const router = createBrowserRouter([
-  {path: "/", element: <App/>},
-  {path: "/projects", element: <AllProjects/>},
-  {path: "/about", element: <AllAbout/>},
-  {path: "/guestbook", element: <Guestbook/>},
-  {path: "/contact", element: <AllContacts/>},
+const router = createBrowserRouter(
+  [
+    { path: "/", element: <App/> },
+    { path: "/projects", element: <AllProjects/> },
+    { path: "/about", element: <AllAbout/> },
+    { path: "/guestbook", element: <Guestbook/> },
+    { path: "/contact", element: <AllContacts/> },
+  ],
   {
-    basename: "/MyPortfolio",
+    basename: "/MyPortfolio"
   }
-])
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
