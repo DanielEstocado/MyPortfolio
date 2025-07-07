b9ed72f5cc5d2bd58e3f1dcf44f1a4b2f2b8cde3
Daniel Estocado <estocado.daniel143@gmail.com>
2025-07-07 10:32:03 +0800

    Install and Configure github pages

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,9 @@ const router = createBrowserRouter([
   {path: "/about", element: <AllAbout/>},
   {path: "/guestbook", element: <Guestbook/>},
   {path: "/contact", element: <AllContacts/>},
+  {
+    basename: "/MyPortfolio",
+  }
 ])
 
 createRoot(document.getElementById('root')).render(
