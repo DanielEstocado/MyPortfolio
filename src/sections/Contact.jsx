2d643173a09252ec2e5dc14eaa7fd7743dc22a40
Daniel Estocado <estocado.daniel143@gmail.com>
2025-07-07 08:55:18 +0800

    Working contact email

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,14 +1,40 @@
 import React, { useState } from 'react'
 import Button from '../assets/components/Button'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
   const [email, setEmail] = useState("")
   const [message, setMessage] = useState("")
+  const [loading, setLoading] = useState(false)
 
-  function handleContactSubmit(e) {
-    e.preventDefault()
-    console.log("pressed")
+  const handleContactSubmit = async (e) => {
+    e.preventDefault() 
+
+    const serviceID = import.meta.env.VITE_EMAILJS_SERVICE_ID
+    const templateID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID
+    const publicKey =  import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+
+    const templateParams = {
+      from_name: "A Portfolio Client",
+      from_email: email,
+      to_name: "Daniel Estocado", 
+      message: message,
+      title: "MyPortfolio"
+    }
+
+  setLoading(true)
+  try {
+    const response = await emailjs.send(serviceID, templateID, templateParams, publicKey)
+    console.log("Email sent successfully: ", response)
+    setEmail("")
+    setMessage("")
+  } catch (error) {
+    console.error("Error sending email: ", error)
+  } finally {
+    setLoading(false)
   }
+}
+
   return (
     <div className=' bg-base-200 shadow-xl lg:rounded-2xl'>
       
@@ -29,7 +55,21 @@ const Contact = () => {
           <h2 className=' uppercase font-montserrat font-bold text-2xl title-gradient mt-8 mb-3'>Your message</h2>
           <textarea className="textarea rounded-md w-full text-lg h-44 px-6 py-4" placeholder="Enter your message..." value={message} onChange={(e)=>{setMessage(e.target.value)}} required/>
           <div className='flex justify-end items-center pt-10'>
-            <button type='submit' className='rounded-xs px-5 py-3 overflow-hidden group my-button'>Send</button>
+            <button type='submit' className='rounded-xs px-5 py-3 overflow-hidden group my-button gap-2 flex items-center justify-center disabled:opacity-60' disabled={loading}>
+              
+              {loading ? (
+                <>
+                  <span>Sending</span>
+                  <span className="loading loading-spinner loading-xs"></span>
+                </>
+              ):(
+                <>
+                  <span>Send</span>
+                  <i className='fa-solid fa-paper-plane'></i>
+                </>
+              )}
+            </button>
+            
           </div>
         </form> 
       </div>
